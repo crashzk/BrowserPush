c35dda4af2b7ad4780ffeff23e185e9452aafdad
crashzk <crashzk@zkservidores.com>
2026-03-28 08:28:46 -0300

    Create browser-push.php

---
+++ Resources/lang/br/browser-push.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'banner_title' => 'Fique por dentro',
+    'banner_text' => 'Receba notificações instantâneas sobre atualizações importantes.',
+    'enable' => 'Ativar',
+    'not_now' => 'Agora não',
+    'enabled' => 'Notificações push ativadas',
+    'disabled' => 'Notificações push desativadas',
+    'error' => 'Falha ao se inscrever nas notificações',
+    'unsupported' => 'Seu navegador não suporta notificações push',
+    'denied' => 'As notificações estão bloqueadas nas configurações do navegador',
+];
